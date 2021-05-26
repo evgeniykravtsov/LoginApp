dddb21633fa8cb6cbf0968939e43ffddc6fa3057
Kravtsov Evgeniy <ekravtsov@ozon.travel>
2021-05-26 21:03:09 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Kravtsov Evgeniy on 26.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
